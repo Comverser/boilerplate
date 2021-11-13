ebec885d08e435833f55e5a48cad94d1bef4ccad
Hyunduk Shin <hyunduk0206@gmail.com>
2021-11-13 22:02:34 +0900

    -

--- public/calculate.ts
+++ public/calculate.ts
@@ -1,1 +1,8 @@
-console.log("Hellot");
+// fetch("short.json")
+//   .then((res) => res.json())
+//   .then((json) => console.log(json));
+
+import fs from "fs";
+
+let data = fs.readFileSync("./short.json");
+

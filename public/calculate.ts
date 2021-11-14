c3dc5f35376057feba9375b25ef0eafa1736ad42
Hyunduk Shin <hyunduk0206@gmail.com>
2021-11-14 17:35:26 +0900

    -

--- public/calculate.ts
+++ public/calculate.ts
@@ -3,6 +3,14 @@
 //   .then((json) => console.log(json));
 
 import fs from "fs";
+import path from "path";
 
-let data = fs.readFileSync("./short.json");
+let raw: Buffer = fs.readFileSync(path.resolve(__dirname, "./short.json"));
+let data: JSON = JSON.parse(raw.toString() as string);
 
+for (const key in data) {
+  if (Object.prototype.hasOwnProperty.call(data, key)) {
+    const element = data[key];
+    console.log(element);
+  }
+}

3a0d38997f5e1d7e5d446752ed106c5d1702d2fb
Hyunduk Shin <hyunduk0206@gmail.com>
2021-11-14 23:50:27 +0900

    -

--- public/calculate.ts
+++ public/calculate.ts
@@ -5,15 +5,30 @@
 import fs from "fs";
 import path from "path";
 
-
 let raw: Buffer = fs.readFileSync(path.resolve(__dirname, "./long.json"));
-
 let data = JSON.parse(raw.toString() as string);
 
+// the latest day of measurement
 const dataCol3 = data.map((v: any, i: number) => v[2]).slice(1);
-// console.log(dataCol3);
+const uniqueCol3 = Array.from(new Set(dataCol3)).sort().reverse();
+let latestDay: number = uniqueCol3[0] as number;
+while (
+  data.filter((elem: any) => {
+    return elem[2] === latestDay;
+  }).length < 100
+) {
+  latestDay -= 1;
+}
 
-const uniqueCol3 = Array.from(new Set(dataCol3));
-console.log(uniqueCol3);
+// the latest data
+const latestData = data.filter((elem: any) => {
+  return elem[2] === latestDay;
+});
 
+// voltage and time to analyze
+const voltage = latestData.map((v: any) => v[1]);
+const time = latestData.map((v: any) => v[3]);
 
+console.log(voltage);
+console.log(time);
+console.log(data[2]);
